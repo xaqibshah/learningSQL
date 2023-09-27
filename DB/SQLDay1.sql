a66df41fef8b7c4d2a9d70349fc3719e61142305
Saqib Shah <xaqibshah@gmail.com>
2023-09-27 15:52:17 +0500

    create database and tables

--- DB/SQLDay1.sql
+++ DB/SQLDay1.sql
@@ -17,34 +17,64 @@
 -- select * from employeeInfo;
 
 
-CREATE DATABASE networksolutions;
-USE networksolutions;
+-- CREATE DATABASE networksolutions;
+-- USE networksolutions;
 
-CREATE TABLE employeeInfo(
-empID int primary key,
-empName varchar(50),
-empSalary int
+-- CREATE TABLE employeeInfo(
+-- empID int primary key,
+-- empName varchar(50),
+-- empSalary int
+-- );
+
+-- INSERT INTO employeeInfo(
+-- empID, empName, EmpSalary)
+-- values
+-- (01, "adam", 25000),
+-- (02, "bob", 35000),
+-- (03, "casey", 30000),
+-- (04, "ahmed", 20000);
+
+-- SELECT * FROM employeeinfo;
+
+-- CREATE TABLE emp (
+-- id int not null,
+-- salary INT DEFAULT 15000
+-- );
+--  
+--  INSERT INTO emp ( id ) values (01);
+--  INSERT INTO emp ( id ) values (02);
+--   INSERT INTO emp ( id ) values (03);
+--    INSERT INTO emp ( id ) values (04);
+--    
+--    SELECT * FROM emp;
+--    
+
+
+DROP DATABASE collage;
+
+CREATE DATABASE collage;
+USE collage;
+
+CREATE TABLE student(
+rollno INT PRIMARY KEY,
+name VARCHAR(50),
+marks INT NOT NULL,
+grade VARCHAR(1),
+city VARCHAR(20)
 );
 
-INSERT INTO employeeInfo(
-empID, empName, EmpSalary)
-values
-(01, "adam", 25000),
-(02, "bob", 35000),
-(03, "casey", 30000),
-(04, "ahmed", 20000);
+-- INSERT INTO student 
+-- (rollno, name, marks, grade, city) 
+-- VALUES
+-- (101, "saqib", 78, "C" "karachi");
+-- (102, "bilal", 55, "E" "karachi"),
+-- (103, "nadir", 85, "C" "Lahore"),
+-- (104, "uamir", 98, "A" "karachi"),
+-- (105, "asim", 80, "C" "Multan"),
+-- (106, "ali", 68, "A" "Lahore");
 
-SELECT * FROM employeeinfo;
 
-CREATE TABLE emp (
-id int not null,
-salary INT DEFAULT 15000
-);
- 
- INSERT INTO emp ( id ) values (01);
- INSERT INTO emp ( id ) values (02);
-  INSERT INTO emp ( id ) values (03);
-   INSERT INTO emp ( id ) values (04);
-   
-   SELECT * FROM emp;
-   
+INSERT INTO student 
+(rollno, name, marks, grade, city) 
+VALUES
+(101, "anil", 78, "C" "Pune");
